dc023ac1600fd970d930fd276471eddbfba8273b
Foxvorism <imjohani2005@gmail.com>
2025-05-03 22:10:26 +0700

    feat: Create hero component for home page

---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import heroImg from "../../public/img/hero.png";
+
+const Hero: React.FC = () => {
+  return (
+    <>
+      <div
+        className="flex bg-black h-[calc(100vh-100px)] bg-cover"
+        style={{
+          backgroundImage: `url(/img/home/hero.png)`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      ></div>
+    </>
+  );
+};
+
+export default Hero;
