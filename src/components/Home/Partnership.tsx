4db3078f8cb3933095a9e9514eb123f8e3ecc678
Foxvorism <imjohani2005@gmail.com>
2025-05-03 22:10:51 +0700

    feat: Create partnership component for home page

---
+++ src/components/Home/Partnership.tsx
@@ -0,0 +1,33 @@
+const Partnership: React.FC = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center p-14">
+        <div className="px-[10vw]">
+          <div className="grid grid-cols-2 gap-15">
+            <div className="flex justify-center items-center">
+              <div className="text-left w-[22rem]">
+                <h2 className="text-2xl font-bold mb-5">Partnership</h2>
+
+                <p className="mb-5 text-lg">
+                  Atedoz memiliki layanan photobooth yang terbuka untuk
+                  melakukan kerja sama dengan event, rental dan berkolaborasi.
+                </p>
+
+                <div className="mb-8">
+                  <button className="px-10 py-3 rounded-md text-white bg-[var(--color-black-2)] hover:scale-[102%] cursor-pointer">
+                    Pelajari Lebih Lanjut
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <img src="img/home/partnership.png" alt="partnership" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Partnership;
