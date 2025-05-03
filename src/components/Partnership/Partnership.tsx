1ea054f7aec121610a0aa161dddaeb8da6555892
Foxvorism <imjohani2005@gmail.com>
2025-05-03 22:15:22 +0700

    feat: Create partnership component for partnership page

---
+++ src/components/Partnership/Partnership.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Partnership: React.FC = () => {
+  return (
+    <>
+      <div className="p-10 w-full text-center px-50">
+        <h2 className="text-2xl font-bold mb-5">Partnership</h2>
+        <h2 className="text-base font-medium mb-5 px-50">
+          Atedoz adalah penyedia layanan photobooth modern yang siap memberikan
+          pengalaman berfoto yang seru, kreatif, dan berkesan. Kami membuka
+          peluang kerja sama dengan berbagai mitra, mulai dari event organizer,
+          venue, wedding organizer, hingga pelaku usaha di bidang hiburan dan
+          hospitality.
+        </h2>
+      </div>
+    </>
+  );
+};
+
+export default Partnership;
