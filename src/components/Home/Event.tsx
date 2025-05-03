d384fa0dd77feebe970144b2e6e61de6bd9c56d3
Foxvorism <imjohani2005@gmail.com>
2025-05-03 22:10:09 +0700

    feat: Create event component for home page

---
+++ src/components/Home/Event.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Event: React.FC = () => {
+  const event = [
+    { id: 1, img: "img/home/event1.png", alt: "event 1" },
+    { id: 2, img: "img/home/event2.png", alt: "event 2" },
+    { id: 3, img: "img/home/event3.png", alt: "event 3" },
+  ];
+
+  return (
+    <>
+      <div className="flex justify-center items-center p-14">
+        <div className="text-center">
+          <h2 className="font-bold text-3xl mb-10">
+            Acara Terkini dari Photobooth
+          </h2>
+
+          {/* Horizontal Scrollable Image Section */}
+          <div className="w-[60vw] flex overflow-x-auto space-x-5 py-4">
+            {event.map((event) => (
+              <div className="flex-shrink-0" key={event.id}>
+                <img
+                  src={event.img}
+                  alt={event.alt}
+                  className="h-[30rem] object-cover rounded-lg"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Event;
