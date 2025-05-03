a97bb977688a98578b1df428077bfdf9dfbabf4b
Foxvorism <imjohani2005@gmail.com>
2025-05-03 22:04:52 +0700

    feat: Create register component for register page

---
+++ src/components/Auth/RegisterComp.tsx
@@ -0,0 +1,107 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { EyeIcon, EyeCloseIcon } from "@/icons";
+
+const RegisterComp: React.FC = () => {
+  return (
+    <>
+      <div className="grid grid-cols-2">
+        <div className="bg-black">
+          <div
+            className="flex bg-black h-[100vh]"
+            style={{
+              backgroundImage: `url(/img/auth/register.png)`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+          />
+        </div>
+        <div className="flex justify-center items-center px-30">
+          <Link href="/">
+            <button className="cursor-pointer absolute w-7 h-7 rounded-md bg-[var(--color-black-2)] top-10 right-10 text-white font-semibold flex justify-center items-center">
+              <div>X</div>
+            </button>
+          </Link>
+          <div className="w-full">
+            <div className="flex justify-center item-center">
+              <img
+                src="/img/logo-atedoz.png"
+                alt="logo atedoz"
+                className="w-[12rem] mb-10"
+              />
+            </div>
+            <h2 className="font-bold text-4xl text-center mb-5">Register</h2>
+
+            <form action="" className="">
+              <div className="mb-5">
+                <h2 className="mb-1.5 text-sm font-medium text-gray-700">
+                  Full Name
+                </h2>
+                <input
+                  type="nama"
+                  placeholder="Masukan nama lengkap anda"
+                  className="w-full px-3 py-2 text-[14px] rounded-md bg-transparent border text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-3 focus:ring-brand-500/10"
+                />
+              </div>
+
+              <div className="mb-5">
+                <h2 className="mb-1.5 text-sm font-medium text-gray-700">
+                  Email Address
+                </h2>
+                <input
+                  type="email"
+                  placeholder="Masukan email anda"
+                  className="w-full px-3 py-2 text-[14px] rounded-md bg-transparent border text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-3 focus:ring-brand-500/10"
+                />
+              </div>
+
+              <div className="mb-5">
+                <h2 className="mb-1.5 text-sm font-medium text-gray-700">
+                  Password
+                </h2>
+                <input
+                  type="password"
+                  placeholder="Masukan password anda"
+                  className="w-full px-3 py-2 text-[14px] rounded-md bg-transparent border text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-3 focus:ring-brand-500/10"
+                />
+              </div>
+
+              <div className="mb-5">
+                <h2 className="mb-1.5 text-sm font-medium text-gray-700">
+                  Confirm Password
+                </h2>
+                <input
+                  type="password"
+                  placeholder="Tulis ulang password"
+                  className="w-full px-3 py-2 text-[14px] rounded-md bg-transparent border text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-3 focus:ring-brand-500/10"
+                />
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="w-full text-center bg-[var(--color-black-2)] text-white py-2 rounded-md font-semibold cursor-pointer hover:bg-[var(--color-brand-500)]"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+
+            <hr className="my-5 opacity-20" />
+
+            <div className="">
+              <Link href="/login">
+                <button className="w-full text-center bg-white text-[var(--color-black-2)] border-2 py-2 rounded-md font-semibold hover:bg-[var(--color-black-2)] hover:text-white cursor-pointer mb-3">
+                  Sudah punya akun? Login
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RegisterComp;
