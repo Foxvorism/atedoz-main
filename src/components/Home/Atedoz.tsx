a2d0df200095611605c62e8ad06c8191d555e2bf
Foxvorism <imjohani2005@gmail.com>
2025-05-03 22:09:58 +0700

    feat: Create atedoz component for home page

---
+++ src/components/Home/Atedoz.tsx
@@ -0,0 +1,19 @@
+const Atedoz: React.FC = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center p-14 bg-white">
+        <div className="text-center">
+          <h2 className="font-bold text-3xl mb-10">Atedoz Space</h2>
+          <p className="font-medium text-lg w-[60vw]">
+            Atedoz adalah sebuah layanan yang bergerak di jasa photo terbesar di
+            Kota Bogor. Layanan kami memiliki banyak jenis dan yang paling
+            terkenal adalah Photobooth yang telah melayani banyak pelanggan
+            serta menjadi yang terbaik.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Atedoz;
