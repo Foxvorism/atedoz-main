6488e4743e11d6a5cd21ac020d8456e6bd265269
Foxvorism <imjohani2005@gmail.com>
2025-05-23 21:47:36 +0700

    feat: Change language

--- src/components/Home/Partnership.tsx
+++ src/components/Home/Partnership.tsx
@@ -8,7 +8,7 @@ const Partnership: React.FC = () => {
           <div className="grid grid-cols-2 gap-15">
             <div className="flex justify-center items-center">
               <div className="text-left w-[22rem]">
-                <h2 className="text-2xl font-bold mb-5">Partnership</h2>
+                <h2 className="text-2xl font-bold mb-5">Kerja Sama</h2>
 
                 <p className="mb-5 text-lg">
                   Atedoz memiliki layanan photobooth yang terbuka untuk
