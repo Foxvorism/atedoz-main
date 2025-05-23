828772099f3bfbb1065cd9ec3e524c7865e93c4c
Foxvorism <imjohani2005@gmail.com>
2025-05-23 20:39:55 +0700

    feat: Create event detail function

--- src/components/Events/Event.tsx
+++ src/components/Events/Event.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface EventItem {
   id: number;
@@ -23,25 +24,29 @@ const Event: React.FC<EventProps> = ({ events }) => {
         Berikut adalah event-event yang kami selenggarakan
       </h2>
       <div className="flex justify-center items-center">
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-full">
           {sortedEvents.map((event) => (
-            <div
-              key={event.id}
-              className="relative w-full bg-gray-100 rounded-lg overflow-hidden hover:scale-[102%]"
-            >
-              <Image
-                layout="responsive"
-                width={500}
-                height={0}
-                src={event.thumbnail}
-                alt={event.nama_event}
-                className="aspect-video w-full object-cover cursor-pointer"
-              />
-              <div className="p-4">
-                <h2 className="text-xl font-bold mb-3">{event.nama_event}</h2>
-                <h3 className="text-gray-400 text-xs">{event.tanggal_event}</h3>
+            <Link href={`/events/detail/${event.id}`}>
+              <div
+                key={event.id}
+                className="relative w-full h-full bg-gray-100 rounded-lg overflow-hidden hover:scale-[102%]"
+              >
+                <Image
+                  layout="responsive"
+                  width={500}
+                  height={0}
+                  src={event.thumbnail}
+                  alt={event.nama_event}
+                  className="aspect-video w-full object-cover cursor-pointer"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-3">{event.nama_event}</h2>
+                  <h3 className="text-gray-400 text-xs">
+                    {event.tanggal_event}
+                  </h3>
+                </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
